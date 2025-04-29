1122d501b6485a75776762f0737adbcf77c4ebde
Lais <laiszanqueta4@gmail.com>
2025-04-29 16:51:25 -0300

    atualizei a calc

--- calculadora.js
+++ calculadora.js
@@ -1,28 +1,36 @@
 var teclado = require("prompt-sync")();
-var operacoes = (teclado("Qual opera\u00E7\u00E3o voc\u00EA quer calcular?"));
-if (operacoes == "+") {
-    var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
-    var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
-    var mais = numero1 + numero2;
-    console.log("O resultado \u00E9 ".concat(mais));
+var continuar = true;
+while (continuar) {
+    var operacoes = teclado("Qual opera\u00E7\u00E3o voc\u00EA quer calcular? (+, -, *, /) ou digite 'sair' para encerrar: ");
+    if (operacoes == 'sair') {
+        continuar = false;
+        console.log("Calculadora encerrada.");
+    }
+    else if (operacoes == '+') {
+        var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
+        var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
+        var mais = numero1 + numero2;
+        console.log("O resultado \u00E9 ".concat(mais));
+    }
+    else if (operacoes == '-') {
+        var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
+        var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
+        var menos = numero1 - numero2;
+        console.log("O resultado \u00E9 ".concat(menos));
+    }
+    else if (operacoes == '*') {
+        var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
+        var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
+        var mult = numero1 * numero2;
+        console.log("O resultado \u00E9 ".concat(mult));
+    }
+    else if (operacoes == '/') {
+        var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
+        var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
+        var div = numero1 / numero2;
+        console.log("O resultado \u00E9 ".concat(div));
+    }
+    else {
+        console.log("Operação inválida! Tente novamente.");
+    }
 }
-else if (operacoes == "-") {
-    var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
-    var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
-    var menos = numero1 - numero2;
-    console.log("O resultado \u00E9 ".concat(menos));
-}
-else if (operacoes == "*") {
-    var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
-    var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
-    var mult = numero1 * numero2;
-    console.log("O resultado \u00E9 ".concat(mult));
-}
-else if (operacoes == "/") {
-    var numero1 = parseInt(teclado("Digite o primeiro n\u00FAmero: "));
-    var numero2 = parseInt(teclado("Digite o outro n\u00FAmero: "));
-    var div = numero1 / numero2;
-    console.log("O resultado \u00E9 ".concat(div));
-}
-else
-    (console.log("Erro na resposta"));
--- calculadora.ts
+++ calculadora.ts
@@ -1,30 +1,35 @@
 
 let teclado = require(`prompt-sync`)();
 
-let operacoes: string =(teclado(`Qual operação você quer calcular?`));
+let continuar: boolean = true;
 
-if (operacoes == `+`){
-    let numero1: number =parseInt(teclado (`Digite o primeiro número: `));
-    let numero2: number =parseInt(teclado (`Digite o outro número: `));
-    let mais= numero1 + numero2
-    console.log (`O resultado é ${mais}`);
+while (continuar) {
+    let operacoes: string = teclado(`Qual operação você quer calcular? (+, -, *, /) ou digite 'sair' para encerrar: `);
+
+    if (operacoes == 'sair') {
+        continuar = false;
+        console.log("Calculadora encerrada.");
+    } else if (operacoes == '+') {
+        let numero1: number = parseInt(teclado(`Digite o primeiro número: `));
+        let numero2: number = parseInt(teclado(`Digite o outro número: `));
+        let mais = numero1 + numero2;
+        console.log(`O resultado é ${mais}`);
+    } else if (operacoes == '-') {
+        let numero1: number = parseInt(teclado(`Digite o primeiro número: `));
+        let numero2: number = parseInt(teclado(`Digite o outro número: `));
+        let menos = numero1 - numero2;
+        console.log(`O resultado é ${menos}`);
+    } else if (operacoes == '*') {
+        let numero1: number = parseInt(teclado(`Digite o primeiro número: `));
+        let numero2: number = parseInt(teclado(`Digite o outro número: `));
+        let mult = numero1 * numero2;
+        console.log(`O resultado é ${mult}`);
+    } else if (operacoes == '/') {
+        let numero1: number = parseInt(teclado(`Digite o primeiro número: `));
+        let numero2: number = parseInt(teclado(`Digite o outro número: `));
+        let div = numero1 / numero2;
+        console.log(`O resultado é ${div}`);
+    } else {
+        console.log("Operação inválida! Tente novamente.");
+    }
 }
-else if (operacoes == `-`){
-    let numero1: number =parseInt(teclado (`Digite o primeiro número: `));
-    let numero2: number =parseInt(teclado (`Digite o outro número: ` ));
-    let menos= numero1 - numero2
-    console.log(`O resultado é ${menos}`);
-}
-else if (operacoes == `*`){
-    let numero1: number =parseInt(teclado (`Digite o primeiro número: `));
-    let numero2: number =parseInt(teclado (`Digite o outro número: ` ));
-    let mult= numero1 * numero2
-    console.log(`O resultado é ${mult}`);
-}
-else if (operacoes == `/`){
-    let numero1: number =parseInt(teclado (`Digite o primeiro número: `));
-    let numero2: number =parseInt(teclado (`Digite o outro número: ` ));
-    let div= numero1 / numero2
-    console.log(`O resultado é ${div}`);
-}
-else (console.log(`Erro na resposta`));
